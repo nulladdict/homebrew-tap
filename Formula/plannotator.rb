36f0c9c983d8a007cd4871f39b0fbd8218e512d7
nulladdict <nulladdicted@gmail.com>
2026-05-12 09:34:40 +0200

    fix: plannotator version

--- Formula/plannotator.rb
+++ Formula/plannotator.rb
@@ -1,6 +1,6 @@
 class Plannotator < Formula
-  version "0.19.11"
-  sha256 "e96e194bc781f602b410f05b345ebc09c2a55ee560c440923cca932f673c85a3"
+  version "0.19.14"
+  sha256 "f7202d188432956ff42bd8cc97c024d3be9f803698065581e5897e7333a977eb"
   url "https://github.com/backnotprop/plannotator/releases/download/v#{version}/plannotator-darwin-arm64",
       using: :nounzip
 
